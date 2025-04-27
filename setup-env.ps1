c104e57a1685ebed438729b5d2d21ebca0f4bb8b
robbpaulsen <pemmtest.087@gmail.com>
2025-04-27 19:56:11 +0100

    Temporalmente solo se respalda un avance de los perfiles.

--- setup-env.ps1
+++ setup-env.ps1
@@ -14,50 +14,44 @@ https://github.com/robbpaulsen
 
 #Requires -RunAsAdministrator
 
-function chkrole {
-    $IsAdmin = (New-Object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([System.Security.Principal.WindowsBuiltInRole]:: Administrator)
-    if (-not $IsAdmin) {
-        Write-Output "`n⚠️ Necesitas ejecutar el script como Administrador y/o Desbloquear el Script." -ForegroundColor Green -BackgroundColor Black
-        exit 1
-    }
+$IsAdmin = (New-Object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([System.Security.Principal.WindowsBuiltInRole]:: Administrator)
+if (-not $IsAdmin) {
+    Write-Output "`n⚠️ Necesitas ejecutar el script como Administrador y/o Desbloquear el Script." -ForegroundColor Green -BackgroundColor Black
+    exit 1
 }
-chkrole
 
-function Update-PowerShell {
+try {
     Write-Output "`n⏳ Primero Actualizar el Sistema - " -ForegroundColor Yellow -NoNewline; Write-Output "[01]" -ForegroundColor Green -BackgroundColor Black
     $StopWatch = [System.Diagnostics.Stopwatch]::StartNew()
-    try {
-        # Update PowerShell help and set execution policy to Bypass
-        Update-Help -Force
-        Set-ExecutionPolicy Bypass -Force:$True -Confirm:$False -ErrorAction SilentlyContinue
+    # Update PowerShell help and set execution policy to Bypass
+    Update-Help -Force;
+    Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force:$True -ErrorAction SilentlyContinue
 
-        # Install required modules from PSGallery repository
-        $modules = @(
-            'PSWindowsUpdate',
-            'MSCatalog',
-            'TerminalIcons',
-            'oh-my-posh-core',
-            'PSFzf'
-        )
-
-        foreach ($module in $modules) {
-            Install-Module -Name $module -Repository PSGallery -Force
-            Import-Module -Name $module -Scope local -Force
-        }
-
-        # Download and install Windows updates
-        Start-Process -Verb RunAs powershell.exe -ArgumentList '-Version 5.0 -NoExit `
-        -WindowStyle Normal -NoProfile -ExecutionPolicy ByPass -Command Get-WindowsUpdate `
-        -AcceptAll -Download -Install -IgnoreReboot -MicrosoftUpdate -Verbose *>&1 | Out-File `
-        $env:USERPROFILE\PSWindowsUpdate.log'
+    # Install required modules from PSGallery repository
+    $modules = @(
+        'PSWindowsUpdate',
+        'MSCatalog',
+        'TerminalIcons',
+        'oh-my-posh-core',
+        'PSFzf',
+        'BurntToast'
+    )
+    foreach ($module in $modules) {
+        Install-Module -Name $module -Repository PSGallery -Force
+        Import-Module -Name $module -Scope local -Force
     }
-    catch {
-        "`n⚠️ Error in line $($Error[0].InvocationInfo.ScriptLineNumber): $($Error[0])"
-    }
+
+    # Download and install Windows updates
+    Start-Process powershell.exe -WindowStyle Maximized -Verb RunAs -ArgumentList '-Version 5.0' '-NoExit' `
+        '-WindowStyle Maximized' '-NoProfile' '-ExecutionPolicy ByPass' '-Command' 'Get-WindowsUpdate' `
+        '-AcceptAll' '-ForceInstall' '-IgnoreReboot' '-MicrosoftUpdate' '-WindowsUpdate' '-Verbose' '-NoExit'
+    | Out-File '$env:USERPROFILE\PSWindowsUpdate.log'
 }
-Update-PowerShell
+catch {
+    "`n⚠️ Error in line $($Error[0].InvocationInfo.ScriptLineNumber): $($Error[0])"
+}
 
-function Enable-Containers {
+try {
     # Display a progress bar and status message
     $status = "`n⏳ Se Habilitara la plataforma de Contenedores y Virtualización - "
     Write-Host $status -ForegroundColor Yellow -NoNewline
@@ -67,7 +61,9 @@ function Enable-Containers {
 
     # Enable Windows features using Write-Progress and Enable-WindowsOptionalFeature
     Write-Progress -Activity 'Enable Windows Features' -Status "`n⏳ Habilitando Windows SandBox...⏳" `
-    -CurrentOperation (Enable-WindowsOptionalFeature -FeatureName "Containers-DisposableClientVM" -All -Online -NoRestart)
+        -CurrentOperation (Enable-WindowsOptionalFeature -FeatureName "Containers-DisposableClientVM" -All -Online -NoRestart)
     Write-Progress -Completed
 }
-Enable-Containers
+catch {
+    "`n⚠️ Error in line $($Error[0].InvocationInfo.ScriptLineNumber): $($Error[0])"
+}
